04a98c9333cfd75c859471fb5f673627c33d77a7
Moegamad Uzair Jack <jckmoe002@myuct.ac.za>
2025-06-05 08:33:43 +0200

    Add files

---
+++ EstuariesForProtection.R
@@ -0,0 +1,17 @@
+# Marine Spatial Planning
+# Assignment
+# Moegamad Uzair Jack
+
+library(tidyverse)
+library(ggpubr)
+library(readr)
+
+surveydata <- read.csv('EstuaryFishSurveyData.csv')
+
+head(surveydata)
+tail(surveydata)
+glimpse(surveydata)
+structure(surveydata)
+
+view(surveydata)
+
